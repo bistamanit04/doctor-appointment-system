cf8b2dd4623d0ff9c375e09fe4e99f3c72cb7ecc
Manit Bista <Bistamanit04@gmail.com>
2026-09-17 15:32:47 +0545

    enhanced find doc and made it dynamic

--- controllers/FindDoctor_controller.py
+++ controllers/FindDoctor_controller.py
@@ -1,26 +1,45 @@
 from template_engine import TemplateEngine
 from database import get_connection
 
+
 class DoctorController:
+
     @staticmethod
     def DOClist(request):
-        
-        connection=get_connection()
-        cursor=connection.cursor()
-        
+
+        connection = get_connection()
+        cursor = connection.cursor()
+
         cursor.execute("""
-                       select doctor_id, name,specialization,email,phone 
-                       FROM doctor
-                       ORDER by name
+            SELECT
+                doctor.doctor_id,
+                doctor.name,
+                doctor.specialization,
+                doctor.email,
+                doctor.phone,
+                doctor.status,
+                doctor_bio.profile_image,
+                doctor_bio.nmc_no,
+                doctor_bio.experience,
+                doctor_bio.qualification,
+                doctor_bio.location,
+                doctor_bio.about,
+                doctor_bio.consultation_fee
+            FROM doctor
+            LEFT JOIN doctor_bio
+                ON doctor.doctor_id = doctor_bio.doctor_id
+            ORDER BY doctor.name
         """)
-        doctors= cursor.fetchall()
+
+        doctors = cursor.fetchall()
+
         connection.close()
-        
+
         TemplateEngine.render(
             request,
             "FindDoctor.html",
             {
-                "title":"FIND UR DOCTOR",
-                "doctors":doctors
+                "title": "Find Doctors",
+                "doctors": doctors
             }
-        )  
+        )
